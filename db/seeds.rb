16a61fd165059ad42b0de10dd6c65dbf035750c9
Helene Briand <helenebriand01@gmail.com>
2022-11-17 16:44:28 +0100

    2nd ex

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,23 @@
 #
 # Examples:
 #
+require 'faker'
+puts "Creating restaurants..."
+10.times do
+movies = Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: Faker::Internet.url, rating: Faker::Number.between(from: 0.0, to: 1.0))
+end
+puts "Finished"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+###################
+# si on veut faire avec API
+###################
+
+#installer gem "open-uri" dans GemFile
+#require "open-uri"
+# URI.open("https://tmdb.lewagon.com/movie/top_rated").read
+# movies_api = JSON.parse(URI.open("https://tmdb.lewagon.com/movie/top_rated").read)
+# movies_api["results"].each do |movie|
+#     Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: movie["poster_path"],rating: movie["vote_average"])
+# end
